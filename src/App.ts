5bfead5ab58f3eb2c4e28becc5a338f485dd5d96
Anthony Ferrando <ferrando.tony@gmail.com>
2021-04-19 16:02:22 +0200

    Use pagehide, visibilitychange event listeners for page close

--- src/App.ts
+++ src/App.ts
@@ -196,6 +196,35 @@ interface TrackPageData {
   path: string
 }
 
+// See https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+function addOnPageClose(handler: () => any) {
+  let hidden: string | undefined = undefined
+  let visibilityChange: string | undefined = undefined
+  const doc = document as any
+  if (typeof doc.hidden !== "undefined") {
+    // Opera 12.10 and Firefox 18 and later support
+    hidden = "hidden"
+    visibilityChange = "visibilitychange"
+  } else if (typeof doc.msHidden !== "undefined") {
+    hidden = "msHidden"
+    visibilityChange = "msvisibilitychange"
+  } else if (typeof doc.webkitHidden !== "undefined") {
+    hidden = "webkitHidden"
+    visibilityChange = "webkitvisibilitychange"
+  }
+
+  if (hidden) {
+    doc.addEventListener(visibilityChange, () => {
+      if (hidden && doc[hidden]) handler()
+    })
+  } else if ("onpagehide" in window) {
+    // See https://stackoverflow.com/questions/6906146/how-to-detect-browser-support-for-pageshow-and-pagehide
+    window.addEventListener("pagehide", handler)
+  } else {
+    window.addEventListener("unload", handler)
+  }
+}
+
 /**
  * A class that contains a `projectId` and related configuration to track events painlessly.
  */
@@ -276,8 +305,7 @@ export class App {
     // Track the first/current page view
     this.trackSinglePage(true, this.trackPageData.path)
 
-    window.addEventListener("unload", this.trackLastPageTimeSpent)
-
+    addOnPageClose(this.trackLastPageTimeSpent)
     return this.trackPageData.result
   }
 
